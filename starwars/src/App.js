4ff2abde5e8bff7a69b5a4f629f3d277168dc5c1
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-16 10:14:39 -0700

    data passing to container

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,22 +6,22 @@ import CardContainer from './components/CardContainer'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [id, setId] = useState('1')
+  //const [id, setId] = useState('1')
   const [data, setData] = useState({})
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/${id}')
+      .get('https://swapi.co/api/people/1')
       .then(res => {
         
-        setData(data)
+        setData(res.data)
         console.log('res.data = ', res.data);
       })
       .catch(err => console.log('API Request error: ', err))
   }, []);
-
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
