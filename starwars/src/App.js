e316a7a4e9dd3b7a473f0a577e1df6a009e31402
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-16 10:59:19 -0700

    works on one id

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,7 +6,7 @@ import CardContainer from './components/CardContainer'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  //const [id, setId] = useState('1')
+  const [id, setId] = useState('1')
   const [data, setData] = useState({})
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -20,11 +20,21 @@ const App = () => {
         console.log('res.data = ', res.data);
       })
       .catch(err => console.log('API Request error: ', err))
-  }, []);
+  }, [id]);
   
+  // function handleChange(id) {
+  //   axios
+  //     .get('https://swapi.co/api/people')
+  //     .then(res => {
+  //       console.log("people axios call: ", res)
+  //       res.forEach(count => count==id)
+  //     })
+  // }
   return (
     <div className="App">
+      <input value={id} onChange={e => setId(e.target.value)} />
       <h1 className="Header">React Wars</h1>
+      {/* {data.map(item => (<CardContainer data={data} />))} */}
       <CardContainer data={data} />
       {/* <CardContainer name={res.name} species={res.data.species} homeworld={res.data.homeworld} starships={res.data.starships}/> */}
     </div>
--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+// import { Grid } from 'semantic-ui-react'
 import StarCard from './StarCard';
 import '../components/StarWars.css'
 
 export default function CardContainer(props) {
     console.log("CardContainer props: ", props)
+    
     return (
         <div className="card-container">
            <StarCard name={props.data.name} birthYear={props.data.birth_year} eyeColor={props.data.eye_color} hairColor={props.data.hair_color} height={props.data.height}/>
